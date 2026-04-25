27d1ac5ec4bee7e40064efb502248729519c18cb
Stan Robotix <stanrobotix@stanislas.qc.ca>
2026-04-24 21:40:09 -0400

    code of last years leds added in new branch

---
+++ LEDs/src/main/include/Constants.h
@@ -0,0 +1,10 @@
+#include <units/length.h>
+#include <frc/util/Color.h>
+
+namespace LEDsConstants{
+    static constexpr int kLength = 40;
+    constexpr units::meter_t kLedSpacing = 0.03_m;
+    constexpr int kMouthCenter = 20;
+    constexpr int kMaxMouthSize = 10;
+    constexpr int kLEDPort = 8;
+}
