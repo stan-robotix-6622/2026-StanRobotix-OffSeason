1167f5c42ae6e7e8a10b2cee425ea9c5b1ce939c
floragel <floragel@2024@@gmail.com>
2026-05-17 14:35:40 -0400

    Changement des valeurs de Speed dans les constantes

--- Gala/MiniRobot/src/main/include/Constants.h
+++ Gala/MiniRobot/src/main/include/Constants.h
@@ -45,8 +45,8 @@ namespace PIDConstants
 
 namespace DriveConstants
 {
-  constexpr double kSpeed = 0.5;
-  constexpr double kRotationRate = 0.45;
+  constexpr double kSpeed = 0.35;
+  constexpr double kRotationRate = 0.3;
 
 }
 
