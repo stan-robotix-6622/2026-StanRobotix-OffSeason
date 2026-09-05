7a87d4d5152ab2c0a7c330184caf9b3b1a3d88fb
Stan Robotix <stanrobotix@stanislas.qc.ca>
2026-09-04 20:37:44 -0400

    Fix errors from merge

--- Gala/MiniRobot/src/main/cpp/RobotContainer.cpp
+++ Gala/MiniRobot/src/main/cpp/RobotContainer.cpp
@@ -6,30 +6,22 @@
 
 #include <frc2/command/button/Trigger.h>
 #include <frc2/command/Commands.h>
-<<<<<<< HEAD
 #include <frc2/command/button/JoystickButton.h>
 #include <iostream>
-=======
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
 
 #include "commands/Drive.h"
 
 RobotContainer::RobotContainer() {
-<<<<<<< HEAD
   // Initialize all of your commands and subsystems here
-  m_Drivetrain = new SubDrivetrain;
-
-  // mJoystick = new frc::Joystick{OperatorConstants::kDriverJoystickPort};
-=======
   mDrivetrain = new SubDrivetrain;
 
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
+  // mJoystick = new frc::Joystick{OperatorConstants::kDriverJoystickPort};
   // Configure the button bindings
   ConfigureBindings();
 
   	mLED.SetDefaultCommand(mLED.Run([this] {
 	// if (abs(mJoystick->GetX()) > 0.2 || abs(mJoystick->GetY()) > 0.2 || abs(mJoystick->GetZ()) > 0.2) {
-	if (abs(m_XboxController.GetLeftX()) > 0.2 || abs(m_XboxController.GetLeftY()) > 0.2 || abs(m_XboxController.GetRightX()) > 0.2) {
+	if (abs(mXboxController.GetLeftX()) > 0.2 || abs(mXboxController.GetLeftY()) > 0.2 || abs(mXboxController.GetRightX()) > 0.2) {
 		if (!(mLED.mMode == SubLEDs::Mode::moving)) {
 			std::cout << "moving\n";
 		}
@@ -37,7 +29,7 @@ RobotContainer::RobotContainer() {
 	}
 
 	//else if (mJoystick->GetRawButton(4)) { // Bouton Y
-	else if (m_XboxController.Button(4).Get()) { // Bouton Y
+	else if (mXboxController.Button(4).Get()) { // Bouton Y
 		if (!(mLED.mMode == SubLEDs::Mode::waving)) {
 			std::cout << "waving\n";
 		}
@@ -45,7 +37,7 @@ RobotContainer::RobotContainer() {
 	}
 
 	// else if (mJoystick->GetRawButton(6)) { // Bouton RB
-	else if (m_XboxController.Button(6).Get()) { // Bouton RB
+	else if (mXboxController.Button(6).Get()) { // Bouton RB
 		if (!(mLED.mMode == SubLEDs::Mode::test)) {
 			std::cout << "test\n";
 		}
@@ -58,35 +50,16 @@ RobotContainer::RobotContainer() {
 
 void RobotContainer::ConfigureBindings() {
   // Configure your trigger bindings here
-
-<<<<<<< HEAD
-  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-  frc2::Trigger([this] {
-    return m_subsystem.ExampleCondition();
-  }).OnTrue(ExampleCommand(&m_subsystem).ToPtr());
-
   std::cout << "Test\n";
 
-  m_Drivetrain->SetDefaultCommand(frc2::cmd::Run(
-			[this] {
-        double targetSpeed = m_XboxController.GetLeftY();
-        double targetRotation = m_XboxController.GetRightX();
-
-        std::cout << "Left Y : " << targetSpeed << std::endl;
-        std::cout << "Right X : " << targetRotation << std::endl;
-                
-
-        m_currentSpeed += (targetSpeed - m_currentSpeed) * DriveConstants::kSmooth;
-        m_currentRotation += (targetRotation - m_currentRotation) * DriveConstants::kSmooth;
-
-				m_Drivetrain->DriveRobot(
-          -(m_currentSpeed * DriveConstants::kSpeed * (1.2 -m_XboxController.GetRightTriggerAxis())),
-          -(m_currentRotation * DriveConstants::kRotationRate * (1.2 - m_XboxController.GetRightTriggerAxis()))
-=======
   mDrivetrain->SetDefaultCommand(frc2::cmd::Run(
 			[this] {
         double targetSpeed = mXboxController.GetLeftY();
         double targetRotation = mXboxController.GetRightX();
+
+        std::cout << "Left Y : " << targetSpeed << std::endl;
+        std::cout << "Right X : " << targetRotation << std::endl;
+                
 
         mCurrentSpeed += (targetSpeed - mCurrentSpeed) * DriveConstants::kSmooth;
         mCurrentRotation += (targetRotation - mCurrentRotation) * DriveConstants::kSmooth;
@@ -94,7 +67,6 @@ void RobotContainer::ConfigureBindings() {
 				mDrivetrain->DriveRobot(
           -(mCurrentSpeed * DriveConstants::kSpeed * (1.2 -mXboxController.GetRightTriggerAxis())),
           -(mCurrentRotation * DriveConstants::kRotationRate * (1.2 - mXboxController.GetRightTriggerAxis()))
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
         );
 			},
 			{mDrivetrain}));
@@ -103,18 +75,10 @@ void RobotContainer::ConfigureBindings() {
     [this] {
       mDrivetrain->Stop();
     },
-<<<<<<< HEAD
-    {m_Drivetrain}));
-
-  // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
-  // pressed, cancelling on release.
-  m_XboxController.B().WhileTrue(m_subsystem.ExampleMethodCommand());
-
-  m_XboxController.LeftBumper().WhileTrue(DriftL(m_Drivetrain).ToPtr());
-  m_XboxController.RightBumper().WhileTrue(DriftR(m_Drivetrain).ToPtr());
-=======
     {mDrivetrain}));
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
+
+  mXboxController.LeftBumper().WhileTrue(DriftL(mDrivetrain).ToPtr());
+  mXboxController.RightBumper().WhileTrue(DriftR(mDrivetrain).ToPtr());
 }
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
--- Gala/MiniRobot/src/main/include/RobotContainer.h
+++ Gala/MiniRobot/src/main/include/RobotContainer.h
@@ -11,13 +11,10 @@
 
 #include "Constants.h"
 #include "subsystems/SubDrivetrain.h"
-<<<<<<< HEAD
 #include "subsystems/SubLEDs.h"
 #include "commands/Drive.h"
 #include "commands/DriftL.h"
 #include "commands/DriftR.h"
-=======
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
 /**
  * This class is where the bulk of the robot should be declared.  Since
  * Command-based is a "declarative" paradigm, very little robot logic should
@@ -35,23 +32,13 @@ class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   frc2::CommandXboxController mXboxController{OperatorConstants::kDriverControllerPort};
 
-<<<<<<< HEAD
   // The robot's subsystems are defined here...
-  ExampleSubsystem m_subsystem;
-  SubDrivetrain* m_Drivetrain;
+  SubDrivetrain* mDrivetrain;
   SubLEDs mLED;
   
 
-  double m_currentSpeed = 0.0;
-  double m_currentRotation = 0.0;
-
-  frc::Joystick* mJoystick;
-=======
-  SubDrivetrain* mDrivetrain;
-
   double mCurrentSpeed = 0.0;
   double mCurrentRotation = 0.0;
->>>>>>> fc0c6ed111b17d4e621f6d159f8195a26afc2d1d
   
   void ConfigureBindings();
 };
